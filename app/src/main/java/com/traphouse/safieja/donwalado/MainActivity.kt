20d2753dd7a5fd7563c3f5f5022345015b8cc82f
Marcin Safiejko <Marcin.Safiejko@onespan.com>
2021-02-21 16:55:30 +0100

    Added main view

--- app/src/main/java/com/traphouse/safieja/donwalado/MainActivity.kt
+++ app/src/main/java/com/traphouse/safieja/donwalado/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.traphouse.safieja.donwalado
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 
-class MainActivity : AppCompatActivity() {
+
+//49.459069, 13.602941
+//55.383678, 25.182531
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val bounds = LatLngBounds(LatLng(49.459069, 13.602941), LatLng(55.383678, 25.182531))
+//        googleMap?.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 10))
+        googleMap?.setOnMapLoadedCallback {
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 10))
+        }
     }
 }
